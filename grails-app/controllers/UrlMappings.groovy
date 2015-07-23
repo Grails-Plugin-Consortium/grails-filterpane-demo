02651c621d1e788ab5dbf053ffa8f142e9d27dc1
mansiarora <mansi@intelligrape.com>
2015-07-23 17:48:08 +0530

    Updated URLMappings;

--- grails-app/controllers/UrlMappings.groovy
+++ grails-app/controllers/UrlMappings.groovy
@@ -1,13 +1,14 @@
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?" {
+        "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
             }
         }
 
-        "/"(view: "/index")
-        "500"(view: '/error')
+        "/"(view:"/index")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
     }
 }
